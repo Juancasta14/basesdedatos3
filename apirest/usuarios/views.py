ed8020ef32b6642870b1fa692e1ecf0f71d5bd8d
Juancasta14 <162644989+Juancasta14@users.noreply.github.com>
2025-02-21 23:49:39 -0500

    Update views.py
    vistas para consumir las apis

--- apirest/usuarios/views.py
+++ apirest/usuarios/views.py
@@ -18,3 +18,22 @@ class PedidoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pedido.objects.all()
     serializer_class = PedidoSerializer
 
+class ClimaAPIView(APIView):
+    def get(self, request):
+        url = "https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&current_weather=true"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return Response(response.json())  
+        else:
+            return Response({"error": "No se pudo obtener el clima"}, status=500)
+            
+class JorgeAPIView(APIView):
+    def get(self, request):
+        url = "http://18.221.57.79:8000/api/Perfil"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return Response(response.json())  
+        else:
+            return Response({"error": "No se pudo obtener la API de Jorge"}, status=500)           
