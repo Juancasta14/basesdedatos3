3b2e0df663d61e392066ab1105f6e1f1a2f5d77f
Juancasta14 <162644989+Juancasta14@users.noreply.github.com>
2025-02-21 23:51:02 -0500

    Update urls.py
    agregar urls para las apis

--- apirest/usuarios/urls.py
+++ apirest/usuarios/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .routers import UsuarioViewSet, PedidoViewSet
+from .views import ClimaAPIView, JorgeAPIView
 
 router = DefaultRouter()
 router.register(r'usuarios', UsuarioViewSet)
 router.register(r'pedidos', PedidoViewSet)
 
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('clima/', ClimaAPIView.as_view(), name='clima-api'), 
+    path('jorge/', JorgeAPIView.as_view(), name='Jorge-api'), 
+   
 ]
 
