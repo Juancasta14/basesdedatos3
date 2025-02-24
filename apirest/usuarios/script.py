2d8e0b4b6c3bfdb5cf3fa349b24d805c447abcf8
juancasta14 <juanfelipe9902@gmail.com>
2025-02-23 20:12:09 -0500

    Actualizacion de codigo para la validacion de datos

--- apirest/usuarios/script.py
+++ apirest/usuarios/script.py
@@ -1,88 +1,50 @@
 import os
 import django
 import requests
-from .models import Perfil, Productos, Cliente
+from .models import *
 
-def obtener_y_guardar_datos_perfil():
-    urls = "http://18.221.57.79:8000/api/Perfil/"  
-    response = requests.get(urls)   
-
+def sync_data(model, url, mapping):
+    response = requests.get(url)
+    
     if response.status_code == 200:
         data = response.json()
         
         for item in data:
-            # Verificar si ya existe un registro con el mismo 'id'
-            existing_record = Perfil.objects.filter(id=item["id"]).first()
-
-            if existing_record:
-                # Si el registro ya existe, no hace nada
-                print(f"El registro con ID {item['id']} ya existe y no se agrega.")
-            else:
-                # Si el registro no existe, lo agrega
-                Perfil.objects.create(
-                    id=item["id"],  # Asegurándote de que la primary key (id) se use al crear el nuevo registro
-                    nombre_perfil =item["nombre_perfil"]
-                )
+            obj, created = model.objects.update_or_create(
+                id=item["id"], defaults={key: item[value] for key, value in mapping.items()}
+            )
+            
+            if created:
                 print(f"Nuevo registro creado para ID {item['id']}")
-
+            else:
+                print(f"Registro con ID {item['id']} actualizado.")
+        
         print("Proceso completado")
     else:
         print(f"No se pudieron obtener los datos, código de estado: {response.status_code}")
+
+
+
+
+def obtener_y_guardar_datos_perfil():  
+    sync_data(Perfil, "http://18.221.57.79:8000/api/Perfil/", {
+        "nombre_perfil": "nombre_perfil"
+    })
+
+def obtener_y_guardar_datos_clientes():
+    sync_data(Cliente, "http://18.190.176.232:8000/api/clientes/", {
+        "nombre": "nombre",
+        "email": "email"
+    })
         
         
         
 def obtener_y_guardar_datos_productos():
-    urls = "http://3.19.235.79:8000/api/productos/"  # API de cristian
-    response = requests.get(urls)   
-
-    if response.status_code == 200:
-        data = response.json()
-        
-        for item in data:
-            # Verificar si ya existe un registro con el mismo 'id'
-            existing_record = Productos.objects.filter(id=item["id"]).first()
-
-            if existing_record:
-                # Si el registro ya existe, no hace nada
-                print(f"El registro con ID {item['id']} ya existe y no se agrega.")
-            else:
-                # Si el registro no existe, lo agrega
-                Productos.objects.create(
-                    id=item["id"],  # Asegurándote de que la primary key (id) se use al crear el nuevo registro
-                    descripcion =item["descripcion"],
-                    stock =item["stock"],
-                    ubicacion =item["ubicacion"]
-                )
-                print(f"Nuevo registro creado para ID {item['id']}")
-
-        print("Proceso completado")
-    else:
-        print(f"No se pudieron obtener los datos, código de estado: {response.status_code}")
+    sync_data(Productos, "http://3.19.235.79:8000/api/productos/", {
+        "descripcion": "descripcion",
+        "stock": "stock",
+        "ubicacion": "ubicacion"
+    })
         
         
-def obtener_y_guardar_datos_clientes():
-    urls = "http://18.190.176.232:8000/api/clientes/"  
-    response = requests.get(urls)
 
-    if response.status_code == 200:
-        data = response.json()
-        
-        for item in data:
-            # Verificar si ya existe un registro con el mismo 'id'
-            existing_record = Cliente.objects.filter(id=item["id"]).first()
-
-            if existing_record:
-                # Si el registro ya existe, no hace nada
-                print(f"El registro con ID {item['id']} ya existe y no se agrega.")
-            else:
-                # Si el registro no existe, lo agrega
-                Cliente.objects.create(
-                    id=item["id"],  # Asegurándote de que la primary key (id) se use al crear el nuevo registro
-                    nombre =item["nombre"],
-                    email =item["email"]
-                )
-                print(f"Nuevo registro creado para ID {item['id']}")
-
-        print("Proceso completado")
-    else:
-        print(f"No se pudieron obtener los datos, código de estado: {response.status_code}")
